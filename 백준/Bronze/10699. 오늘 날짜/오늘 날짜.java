7eccc452dfc6e0d6e528c6375ae407bae6b6529f
이상민 <94661301+newmin2@users.noreply.github.com>
2022-07-24 20:34:38 +0900

    [Bronze V] Title: 오늘 날짜, Time: 268 ms, Memory: 19456 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter; 
+public class Main {    
+    public static void main(String[] args) {                
+        LocalDate now = LocalDate.now();         
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd"); 
+        String formatedNow = now.format(formatter);
+        System.out.println(formatedNow);
+    }
+}

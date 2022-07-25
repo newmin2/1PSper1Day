251d5288db1d4f44005b8dc9a94485c5a1320249
이상민 <94661301+newmin2@users.noreply.github.com>
2022-07-26 01:22:01 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 128 ms, Memory: 12968 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -7,73 +7,14 @@ public class Main {
         int cnt=0;
         for(int i=0; i<t; i++){
             String s = sc.next();
-            for(int j=0;j<s.length();j++){
-                StringBuilder sb = new StringBuilder();
-                if(s.indexOf(s.charAt(j))==s.lastIndexOf(s.charAt(j)))
+            for(int j=0;j<s.length()-1;j++){
+                if(s.charAt(j)==s.charAt(j+1))
                     continue;
-                String a= s.substring(s.indexOf(s.charAt(j)),s.lastIndexOf(s.charAt(j)));
-                for(int k=0; k<s.lastIndexOf(s.charAt(j))-s.indexOf(s.charAt(j));k++)
-                    sb.append(s.charAt(j));
-                if(!a.equals(sb.toString())){
+                if(s.substring(j+1,s.length()).contains(s.substring(j,j+1))){
                     cnt++;
                     break;}
             }
         }
         System.out.println(t-cnt);
     }
-}
-////////////////////////////////////////////////////////////////////////////////////////////////
-출처: https://st-lab.tistory.com/69
-import java.util.Scanner;
- 
-public class Main {
- 
-	static Scanner in = new Scanner(System.in);
- 
-	public static void main(String[] args) {
- 
-		int count = 0;
-		int N = in.nextInt();
- 
-		for (int i = 0; i < N; i++) {
-			if (check() == true) {
-				count++;
-			}
-		}
-		System.out.println(count);
-	}
- 
-	public static boolean check() {
-		boolean[] check = new boolean[26];
-		int prev = 0;
-		String str = in.next();
-		
-		for(int i = 0; i < str.length(); i++) {
-			int now = str.charAt(i);	// i 번째 문자 저장 (현재 문자)
-			
-			
-			// 앞선 문자와 i 번째 문자가 같지 않다면?
-			if (prev != now) {		
-				
-				// 해당 문자가 처음 나오는 경우 (false 인 경우)
-				if ( check[now - 'a'] == false ) {
-					check[now - 'a'] = true;		// true 로 바꿔준다
-					prev = now;					// 다음 턴을 위해 prev 도 바꿔준다 
-				}
-	 
-				// 해당 문자가 이미 나온 적이 있는 경우 (그룹단어가 아니게 됨) 
-				else {
-					return false;	//함수 종료
-				}
-			}
-	        
-	        
-			// 앞선 문자와 i 번째 문자가 같다면? (연속된 문자)
-			// else 문은 없어도 됨
-			else {
-				continue;
-			}
-		}    
-		return true;
-	}
 }

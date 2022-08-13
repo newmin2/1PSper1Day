a31903bed3192aef4a3f1991a633bce496d1249b
이상민 <94661301+newmin2@users.noreply.github.com>
2022-08-14 00:57:37 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 72 ms, Memory: 11448 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[9];
+        int sum=0;
+        for(int i=0; i<9; i++){
+            arr[i]=Integer.parseInt(bf.readLine());
+            sum+=arr[i];
+        }
+        Arrays.sort(arr);
+        loop:
+        for(int i=0; i<8; i++){
+            for(int j=i+1; j<9; j++){
+                if(sum-arr[i]-arr[j]==100){
+                    for(int k=0; k<9; k++){
+                        if(k==i||k==j)
+                            continue;
+                        System.out.println(arr[k]);
+                    }
+                    break loop;
+                }
+            }
+        }
+    }
+}

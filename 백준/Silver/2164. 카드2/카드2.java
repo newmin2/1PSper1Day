1e4e089846c5ca80a60d0cf04c3bc879ea5d154a
이상민 <94661301+newmin2@users.noreply.github.com>
2022-08-21 01:47:38 +0900

    [Silver IV] Title: 카드2, Time: 80 ms, Memory: 11516 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,28 +1,18 @@
-import java.util.Queue;
-import java.util.LinkedList;
- 
-import java.util.Scanner;
- 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 public class Main {
- 
-	public static void main(String[] args) {
-		
-		Scanner in = new Scanner(System.in);
-		
-		Queue<Integer> q = new LinkedList<>();
-		
-		int N = in.nextInt();
-		
-		for(int i = 1; i <= N; i++) {
-			q.offer(i);
-		}
-		
-		
-		while(q.size() > 1) {
-			q.poll();	// 맨 앞의 원소 버림 
-			q.offer(q.poll());	// 맨 앞의 원소를 버림과 동시에 버려진 원소를 맨 뒤에 삽입 
-		}
-		
-		System.out.println(q.poll());	// 마지막으로 남은 원소 출력 
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+        if(n==1){
+            System.out.println(1);
+            return;}
+        int a = 1;
+        while (n > a) {
+            a *= 2;
+        }
+        a/=2;
+        System.out.println((n-a)*2);
+    }
 }

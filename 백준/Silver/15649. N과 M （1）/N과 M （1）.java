a17acf06f03f45feb40abedb4b199914dc4bdaa1
이상민 <94661301+newmin2@users.noreply.github.com>
2022-08-30 02:28:18 +0900

    [Silver III] Title: N과 M (1), Time: 176 ms, Memory: 21072 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static void nnm(int depth){
+        if (depth == m) {
+            for (int i : arr) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                arr[depth] = i + 1;
+                nnm(depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+    static int[] arr;
+    static boolean[] visited;
+    static int n;
+    static int m;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf= new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr= new int[m];
+        visited = new boolean[n];
+        nnm(0);
+        System.out.println(sb);
+    }
+}

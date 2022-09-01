3f0d2e9b08455f932f1b0ba688ff7bd893ba5c82
이상민 <94661301+newmin2@users.noreply.github.com>
2022-09-02 01:29:41 +0900

    [Silver II] Title: N과 M (12), Time: 116 ms, Memory: 14508 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+    static void nnm(int a,int depth) {
+        if (depth == m) {
+            StringBuilder sb2 = new StringBuilder();
+            for (int i = 0; i < m; i++) {
+                sb2.append(arr[fake[i]]).append(' ');
+            }
+            if (!set.contains(sb2.toString())) {
+                sb.append(sb2).append('\n');
+                set.add(sb2.toString());
+            }
+            return;
+        }
+        for (int i = a; i < n; i++) {
+            fake[depth] = i;
+            nnm(i,depth + 1);
+        }
+    }
+
+    static int[] arr, fake;
+    static HashSet<String> set = new HashSet<>();
+    //static boolean[] visited;
+    static int n, m;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        fake = new int[m];
+        st = new StringTokenizer(bf.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        //visited = new boolean[n];
+        nnm(0, 0);
+        System.out.println(sb);
+    }
+}

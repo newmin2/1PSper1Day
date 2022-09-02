89ea43dd404d4bb61c806c2fd390b445d0070312
이상민 <94661301+newmin2@users.noreply.github.com>
2022-09-03 03:03:17 +0900

    [Bronze V] Title: 카드 게임, Time: 80 ms, Memory: 11480 KB -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf= new BufferedReader(new InputStreamReader(System.in));
+        int ans=0;
+        for(int i=0;i<5;i++){
+        ans+=Integer.parseInt(bf.readLine());}
+        System.out.println(ans);
+    }
+}

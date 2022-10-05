c30549d99cff1dab78a1891fb8064a820d9d646c
이상민 <94661301+newmin2@users.noreply.github.com>
2022-10-06 01:57:04 +0900

    [Silver I] Title: RGB거리, Time: 88 ms, Memory: 11996 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -7,17 +7,20 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int n= Integer.parseInt(bf.readLine());
-        int[][] dp = new int[n][3];
+        int[][] dp = new int[2][3];
         StringTokenizer st =new StringTokenizer(bf.readLine());
         dp[0][0]=Integer.parseInt(st.nextToken());
         dp[0][1]=Integer.parseInt(st.nextToken());
         dp[0][2]=Integer.parseInt(st.nextToken());
         for(int i=1;i<n;i++){
             st=new StringTokenizer(bf.readLine());
-            dp[i][0]=Math.min(dp[i-1][1],dp[i-1][2])+Integer.parseInt(st.nextToken());
-            dp[i][1]=Math.min(dp[i-1][0],dp[i-1][2])+Integer.parseInt(st.nextToken());
-            dp[i][2]=Math.min(dp[i-1][1],dp[i-1][0])+Integer.parseInt(st.nextToken());
+            dp[1][0]=Math.min(dp[0][1],dp[0][2])+Integer.parseInt(st.nextToken());
+            dp[1][1]=Math.min(dp[0][0],dp[0][2])+Integer.parseInt(st.nextToken());
+            dp[1][2]=Math.min(dp[0][1],dp[0][0])+Integer.parseInt(st.nextToken());
+            dp[0][0]=dp[1][0];
+            dp[0][1]=dp[1][1];
+            dp[0][2]=dp[1][2];
         }
-        System.out.println(Math.min(dp[n-1][2],Math.min(dp[n-1][0],dp[n-1][1])));
+        System.out.println(Math.min(dp[1][2],Math.min(dp[1][0],dp[1][1])));
     }
 }

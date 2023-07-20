63db58adfc5f6e4334dded1800c0f85f017ff0d6
이상민 <94661301+newmin2@users.noreply.github.com>
2023-07-20 20:48:33 +0900

    [Gold I] Title: 최솟값과 최댓값, Time: 792 ms, Memory: 78516 KB -BaekjoonHub

---
+++ 백준/Gold/2357. 최솟값과 최댓값/최솟값과 최댓값.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    static class SegmentTree{
+        private int[] minTree;
+        private int[] maxTree;
+        private int treeSize;
+        SegmentTree(int n,int[] arr){
+            int treeheight = (int) (Math.ceil(Math.log(n)/Math.log(2))+1);
+            treeSize = 1<<treeheight;
+            minTree = new int[treeSize];
+            maxTree = new int[treeSize];
+            minInit(arr,1,1,arr.length-1);
+            maxInit(arr,1,1,arr.length-1);
+        }
+        int minInit(int[] arr, int node, int start, int end){
+            if(start==end) return minTree[node]=arr[start];
+            return minTree[node]=Math.min(minInit(arr,node*2,start,(start+end)/2),
+                    minInit(arr,node*2+1,(start+end)/2+1,end));
+        }
+        int maxInit(int[] arr, int node, int start, int end){
+            if(start==end) return maxTree[node]=arr[start];
+            return maxTree[node]=Math.max(maxInit(arr,node*2,start,(start+end)/2),
+                    maxInit(arr,node*2+1,(start+end)/2+1,end));
+        }
+        int findMin(int node,int start,int end, int left, int right){
+            if(left>end||start>right) return Integer.MAX_VALUE;
+            if(left<=start&&right>=end) return minTree[node];
+            return Math.min(findMin(node*2,start,(start+end)/2,left,right),findMin(node*2+1,(start+end)/2+1,end,left,right));
+        }
+        int findMax(int node,int start,int end, int left, int right){
+            if(left>end||start>right) return Integer.MIN_VALUE;
+            if(left<=start&&right>=end) return maxTree[node];
+            return Math.max(findMax(node*2,start,(start+end)/2,left,right),findMax(node*2+1,(start+end)/2+1,end,left,right));
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String[] info = bf.readLine().split(" ");
+        int[] arr = new int[Integer.parseInt(info[0])+1];
+        for(int i=1;i<=Integer.parseInt(info[0]);i++){
+            arr[i]=Integer.parseInt(bf.readLine());
+        }
+        SegmentTree st = new SegmentTree(arr.length-1,arr);
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<Integer.parseInt(info[1]);i++){
+            String[] mInfo = bf.readLine().split(" ");
+            sb.append(st.findMin(1,1,arr.length-1,Integer.parseInt(mInfo[0]),Integer.parseInt(mInfo[1]))).append(" ").append(st.findMax(1,1,arr.length-1,Integer.parseInt(mInfo[0]),Integer.parseInt(mInfo[1]))).append('\n');
+        }
+        System.out.println(sb);
+    }
+}

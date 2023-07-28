6669dbaca89d2f9f7166dc85df4e9d550ba68e23
이상민 <94661301+newmin2@users.noreply.github.com>
2023-07-29 02:59:32 +0900

    [Gold IV] Title: 트리의 지름, Time: 388 ms, Memory: 27884 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.java
@@ -5,6 +5,7 @@ import java.util.*;
 
 public class Main {
     static int max=Integer.MIN_VALUE;
+    static int idx;
     static class Tree{
         int to,value;
         Tree(int to, int value){
@@ -14,6 +15,7 @@ public class Main {
     }
     static boolean[] visited;
     static void dfs(int start, int sum){
+
         for(int i=0;i<list[start].size();i++){
             Tree tree = list[start].get(i);
             if(visited[tree.to])continue;
@@ -21,33 +23,28 @@ public class Main {
             dfs(tree.to, sum+tree.value);
             visited[start]=false;
         }
-        max= Math.max(max,sum);
+        if(sum>max){
+            max=sum;
+            idx=start;
+        }
     }
     static List<Tree>[] list;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int N =Integer.parseInt(bf.readLine());
-        if(N==1){
-            System.out.println(0);
-            return;
-        }
         int[] info;
         list = new ArrayList[N+1];
         visited=new boolean[N+1];
         for(int i=1;i<=N;i++) list[i]=new ArrayList<>();
-        Queue<Integer>q = new ArrayDeque<>();
         for(int i=0;i<N-1;i++) {
             info = Arrays.stream(bf.readLine().split(" "))
                     .mapToInt(Integer::parseInt).toArray();
-            q.offer(info[1]);
-            if(q.peek()==info[0]) q.poll();
             list[info[0]].add(new Tree(info[1],info[2]));
             list[info[1]].add(new Tree(info[0],info[2]));
         }
-        while(!q.isEmpty()){
-            dfs(q.poll(),0);
-        }
+        dfs(1,0);
+        dfs(idx,0);
         System.out.println(max);
     }
 }

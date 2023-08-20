2693dbfdbba4530a1a8187873f5b5abc476186d1
이상민 <94661301+newmin2@users.noreply.github.com>
2023-08-21 00:21:10 +0900

    [Bronze V] Title: 검증수, Time: 76 ms, Memory: 11552 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int sum=0;
+        for(int i=0;i<5;i++) {
+            sum += (int) Math.pow(Integer.parseInt(st.nextToken()), 2);
+        }
+        System.out.println(sum%10);
+    }
+}

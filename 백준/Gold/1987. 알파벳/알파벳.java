22cac059366ca8f00164d1d6f4aef404dbd7cef3
이상민 <94661301+newmin2@users.noreply.github.com>
2024-04-09 17:06:33 +0900

    [Gold IV] Title: 알파벳, Time: 980 ms, Memory: 12512 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,39 +1,40 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
+public class Main {
     static int[] dr = {-1,1,0,0};
     static int[] dc = {0,0,-1,1};
-    static boolean[] alphabet;
+    static boolean[] check;
     static char[][] arr;
-    static int r,c;
-    static int max = Integer.MIN_VALUE;
-    static void dfs(int x, int y, int cnt){
-        if(alphabet[arr[x][y]-'A']) {
-            max = Math.max(cnt,max);
-            return;
-        }
-        alphabet[arr[x][y]-'A']=true;
+    static int r,c,max;
+
+    static void dfs(int x,int y, int depth){
+        max = Math.max(depth,max);
+        check[arr[x][y]-65]=true;
         for(int i=0;i<4;i++){
-            int nr = dr[i]+x;
-            int nc = dc[i]+y;
+            int nr = x+dr[i];
+            int nc = y+dc[i];
             if(nr<0||nc<0||nr>=r||nc>=c)continue;
-            dfs(nr,nc,cnt+1);
+            if(check[arr[nr][nc]-65])continue;
+            dfs(nr,nc,depth+1);
         }
-        alphabet[arr[x][y]-'A']=false;
+        check[arr[x][y]-65]=false;
     }
-    public static void main(String[] args) throws IOException{
+
+    public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
         r = Integer.parseInt(st.nextToken());
         c = Integer.parseInt(st.nextToken());
         arr = new char[r][c];
-        for(int i=0;i<r;i++) {
+        for(int i=0;i<r;i++){
             String s = bf.readLine();
-            arr[i] = s.toCharArray();
+            for(int j=0;j<c;j++){
+                arr[i][j]=s.charAt(j);
+            }
         }
-        alphabet = new boolean[26];
-        dfs(0,0,0);
+        check = new boolean[26];
+        dfs(0,0,1);
         System.out.println(max);
     }
 }

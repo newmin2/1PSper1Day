0b582e7f0c2a76bb12a8728f483828e870c3192b
이상민 <94661301+newmin2@users.noreply.github.com>
2024-05-09 20:22:27 +0900

    [Silver III] Title: 돌 게임 3, Time: 80 ms, Memory: 11512 KB -BaekjoonHub

---
+++ 백준/Silver/9657. 돌 게임 3/돌 게임 3.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+        System.out.println(n%7==2||n%7==0?"CY":"SK");
+    }
+}
